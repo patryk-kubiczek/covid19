2a229ee77d7c3f9299868b0658dbc6846745ffbf
Patryk Kubiczek <patryk.kubiczek@pik-potsdam.de>
2020-03-10 23:03:12 +0100

    Update 10.03 evening

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 ## COVID-19 cases in Germany
 
-The repository contains scripts to download actual and historic data on COVID-19 cases in Germany from Robert Koch Institute (https://www.rki.de/DE/Content/InfAZ/N/Neuartiges_Coronavirus/Fallzahlen.html) and to do some basic analysis. The scripts can be run from `data-analysis.ipynb`. Or you can just download the scraped [dataset](covid19-germany-lands.csv). 
+The repository contains scripts to download actual and historic data on COVID-19 cases in Germany from Robert Koch Institute (https://www.rki.de/DE/Content/InfAZ/N/Neuartiges_Coronavirus/Fallzahlen.html) and to do some basic analysis. The scripts can be run from `covid19-germany.ipynb`. Or you can just download the scraped [dataset](covid19-germany-lands.csv). 
 
-![Cases of COVID-19 in Germany](covid19-cases-Germany.png)
+![Cases of COVID-19 in Germany](covid19-germany.png)
--- scrape_data.py
+++ scrape_data.py
@@ -6,11 +6,14 @@ import time
 from parse import parse
 import pickle
 
-def load_page(datetime):
+def load_page(datetime, current=False):
     formatted_datetime = datetime.strftime("%Y%m%d%H%M%S")
     original_url = "https://www.rki.de/DE/Content/InfAZ/N/Neuartiges_Coronavirus/Fallzahlen.html"
     url = f"https://web.archive.org/web/{formatted_datetime}/{original_url}"
-    page = requests.get(url)
+    if current:
+        page = requests.get(original_url)
+    else:
+        page = requests.get(url)
     print(f"Requested page from {datetime}. Status code: {page.status_code}")
     return page
 
@@ -19,22 +22,21 @@ def parse_page(page):
     soup = BeautifulSoup(page.content, 'html.parser')
     tables = soup.find_all('table')
     print(f"Found {len(tables)} table(s)")
-    df = pd.read_html(str(tables[0]))[0]
+    table = str(tables[0]).replace(".", "")
+    df = pd.read_html(table)[0]
     par = ""
     for p in soup.find_all('p'):
         if ("Stand:" in p.get_text() or "Datenstand:" in p.get_text()) and "Uhr" in p.get_text():
-            par = p.get_text()
+            par = str(p)
             break
-    if par.startswith("(") and par.endswith(")"):
-        par = par[1:-1]
-    timeinfo = parse("Stand: {}, {} Uhr", par)
-    print(par)
+    timeinfo = parse("{}Stand: {}, {} Uhr{}", par)
     if timeinfo is None:
-        timeinfo = parse("Datenstand: {}, {} Uhr", par)
-    date, time = timeinfo[0], timeinfo[1]
+        timeinfo = parse("{}Datenstand: {}, {} Uhr{}", par)
+    date, time = timeinfo[1], timeinfo[2]
     day, month, year = [int(s) for s in date.split(".")]
     hour, minutes = [int(s) for s in time.split(":")]
     dt = datetime(year, month, day, hour, minutes)
+    print(f"Found data from {date}, {time}")
     return df, dt
 
 def query(initial_dt, delta_dt):
@@ -55,6 +57,12 @@ def query(initial_dt, delta_dt):
             dt_list.append(dt)
         query_dt += delta_dt
         time.sleep(1)
+        
+    page = load_page(datetime.now(), current=True)
+    df, dt = parse_page(page)
+    if dt not in dt_list:
+        df_list.append(df)
+        dt_list.append(dt)
 
     with open("data.pkl", "wb") as file:
         pickle.dump([df_list, dt_list], file)
